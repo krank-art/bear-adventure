a86ae3d533f76c56818ca59f535b95105f44f72d
Krank <kranko.art@gmail.com>
2025-07-01 11:37:13 +0200

    Init project

---
+++ index.js
@@ -0,0 +1,18 @@
+import { rollup } from 'rollup';
+import createRollupConfig from './rollup.config.js';
+
+async function build({ outputDir }) {
+  const rollupConfig = await createRollupConfig({ outputDir });
+  const bundle = await rollup(rollupConfig);
+  await bundle.write(rollupConfig.output);
+  console.log('Rollup completed successfully.');
+}
+
+export default async function buildAssets({ outputDir = "dist"}) {
+  await build({ outputDir }).catch((error) => {
+    console.error('Rollup encountered an error:', error);
+    process.exit(1);
+  });
+}
+
+await (() => buildAssets({}))();
---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import copy from 'rollup-plugin-copy';
+
+export default ({ outputDir }) => {
+  return {
+    input: 'lib/main.js',
+    output: {
+      file: outputDir + "/bundle.js",
+      format: 'iife',
+    },
+    plugins: [
+      copy({
+        targets: [
+          { src: 'static/*', dest: outputDir },
+        ],
+      }),
+    ],
+  }
+};
